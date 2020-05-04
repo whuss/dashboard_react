f51834eff1501dcb7dfa6da687dc7b47a6a5ee67
Wilfried Huss <Wilfried.Huss@bartenbach.com>
2020-05-04 12:52:39 +0200

    show restarts of the last two weeks

--- db.py
+++ db.py
@@ -1125,7 +1125,7 @@ class Errors(object):
         sq_device = db.session.query(DeviceInfo.device).subquery()
 
         query = db.session.query(vp.device, vp.timestamp, vp.version_timestamp, vp.branch, vp.commit, vp.ip) \
-            .filter(vp.timestamp >= datetime.now() - timedelta(days=2)) \
+            .filter(vp.timestamp >= datetime.now() - timedelta(days=14)) \
             .outerjoin(sq_device, sq_device.c.device == vp.device) \
             .order_by(vp.device)
 
