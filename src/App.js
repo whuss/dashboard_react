f6776bcc47aa39d934047dcb3fad9d21fd5f01af
Wilfried Huss <husswilfried@gmail.com>
2020-05-06 15:37:50 +0200

    add some bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Toast from "react-bootstrap/Toast";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Table from "react-bootstrap/Table";
+
+import "./App.css";
+
+function Navigation() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">REPRO-LIGHT</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
+
+function ExTable() {
+    return (
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </Table>
+    );
+}
+
+function Example() {
+    return (
+        <Alert dismissible variant="danger">
+            <Alert.Heading>Error!</Alert.Heading>
+            <p>
+                Change and <i>try</i> again.
+            </p>
+        </Alert>
+    );
+}
+
+const ExampleToast = ({ children }) => {
+    const [show, toggleShow] = useState(true);
+
+    return (
+        <>
+            {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+            <Toast show={show} onClose={() => toggleShow(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">React-Bootstrap</strong>
+                </Toast.Header>
+                <Toast.Body>{children}</Toast.Body>
+            </Toast>
+            <Example />
+        </>
+    );
+};
+
+const App = () => (
+    <Container className="p-3">
+        <Navigation />
+        <h1>Dashboard</h1>
+        <ExTable />
+        <Jumbotron>
+            <h1 className="header">Welcome To React-Bootstrap</h1>
+            <ExampleToast>
+                We now have Bootstrap
+                <span role="img" aria-label="tada">
+                    🎉
+                </span>
+            </ExampleToast>
+        </Jumbotron>
+    </Container>
+);
 
 export default App;
