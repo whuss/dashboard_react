9af60bf95a6222006c167a572e71e54d30dd0a4b
Wilfried Huss <husswilfried@gmail.com>
2020-05-07 11:09:41 +0200

    move BokehPlots to module

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //import ScriptTag from "react-script-tag";
 
 import Navigation from "./Navigation";
+import { Plot, PlotDevice } from "./BokehPlot";
 
 import "./App.css";
 
@@ -64,67 +65,6 @@ function AppRouter() {
             </Container>
         </Router>
     );
-}
-
-function Plot(props) {
-    const instance = useRef(null);
-
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [plot, setPlot] = useState(null);
-
-    useEffect(() => {
-        fetch(props.src)
-            .then((res) => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setPlot(result);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            );
-
-        return () => {
-            setPlot(null);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (plot)
-        {
-            console.log("Plot: fully loaded: ", plot);
-            const scriptTag = document.createElement("script");
-            scriptTag.text = plot.script;
-            instance.current.appendChild(scriptTag);
-        }
-        else
-        {
-            console.log("Plot: deleted: ", props.src);
-        }
-    }, [plot]);
-
-    useEffect(() => {
-        console.log("Plot: src changed: ", props.src);
-    }, [props.src]);
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading ...</div>;
-    } else if (plot) {
-        return (
-            <>
-                {/* <ScriptTag type="text/javascript">{plot.script}</ScriptTag> */}
-                <div className={plot.class} id={plot.id} data-root-id={plot.data_root_id}></div>
-                <div ref={instance} />
-            </>
-        );
-    } else {
-        return <div>Plot is null</div>;
-    }
 }
 
 function ExTable() {
@@ -251,69 +191,6 @@ function DeviceState(props) {
     }
 }
 
-function PlotSwitchCycle(props) {
-    const instance = useRef(null);
-
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [plot, setPlot] = useState(null);
-
-    useEffect(() => {
-        const url = props.src + "/" + props.device;
-        console.log("Plot: fetch url: ", url);
-        fetch(url)
-            .then((res) => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setPlot(result);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            );
-
-        return () => {
-            setPlot(null);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (plot)
-        {
-            console.log("Plot: fully loaded: ", plot);
-            const scriptTag = document.createElement("script");
-            scriptTag.text = plot.script;
-            instance.current.appendChild(scriptTag);
-        }
-        else
-        {
-            console.log("Plot: deleted: ", props.src);
-        }
-    }, [plot]);
-
-    useEffect(() => {
-        console.log("Plot: src changed: ", props.src);
-    }, [props.src]);
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading ...</div>;
-    } else if (plot) {
-        return (
-            <>
-                {/* <ScriptTag type="text/javascript">{plot.script}</ScriptTag> */}
-                <div className={plot.class} id={plot.id} data-root-id={plot.data_root_id}></div>
-                <div ref={instance} />
-            </>
-        );
-    } else {
-        return <div>Plot is null</div>;
-    }
-}
-
 function SwitchCycles() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
@@ -352,7 +229,7 @@ function SwitchCycles() {
                     {devices.map((device) => (
                         <tr key={device}>
                             <th>{device}</th>
-                            <td><PlotSwitchCycle src={"/backend/plot_switch_cycle"} device={device}/></td>
+                            <td><PlotDevice src={"/backend/plot_switch_cycle"} device={device}/></td>
                             <td><Button>Download</Button></td>
                         </tr>
                     ))}
---
+++ src/BokehPlot.js
@@ -0,0 +1,128 @@
+import React, { Component, useState, useEffect, useRef } from "react";
+//import ScriptTag from "react-script-tag";
+
+function Plot(props) {
+    const instance = useRef(null);
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [plot, setPlot] = useState(null);
+
+    useEffect(() => {
+        fetch(props.src)
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setPlot(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+
+        return () => {
+            setPlot(null);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (plot)
+        {
+            console.log("Plot: fully loaded: ", plot);
+            const scriptTag = document.createElement("script");
+            scriptTag.text = plot.script;
+            instance.current.appendChild(scriptTag);
+        }
+        else
+        {
+            console.log("Plot: deleted: ", props.src);
+        }
+    }, [plot]);
+
+    useEffect(() => {
+        console.log("Plot: src changed: ", props.src);
+    }, [props.src]);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading ...</div>;
+    } else if (plot) {
+        return (
+            <>
+                {/* <ScriptTag type="text/javascript">{plot.script}</ScriptTag> */}
+                <div className={plot.class} id={plot.id} data-root-id={plot.data_root_id}></div>
+                <div ref={instance} />
+            </>
+        );
+    } else {
+        return <div>Plot is null</div>;
+    }
+}
+
+function PlotDevice(props) {
+    const instance = useRef(null);
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [plot, setPlot] = useState(null);
+
+    useEffect(() => {
+        const url = props.src + "/" + props.device;
+        console.log("Plot: fetch url: ", url);
+        fetch(url)
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setPlot(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+
+        return () => {
+            setPlot(null);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (plot)
+        {
+            console.log("Plot: fully loaded: ", plot);
+            const scriptTag = document.createElement("script");
+            scriptTag.text = plot.script;
+            instance.current.appendChild(scriptTag);
+        }
+        else
+        {
+            console.log("Plot: deleted: ", props.src);
+        }
+    }, [plot]);
+
+    useEffect(() => {
+        console.log("Plot: src changed: ", props.src);
+    }, [props.src]);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading ...</div>;
+    } else if (plot) {
+        return (
+            <>
+                {/* <ScriptTag type="text/javascript">{plot.script}</ScriptTag> */}
+                <div className={plot.class} id={plot.id} data-root-id={plot.data_root_id}></div>
+                <div ref={instance} />
+            </>
+        );
+    } else {
+        return <div>Plot is null</div>;
+    }
+}
+
+export {Plot, PlotDevice};
