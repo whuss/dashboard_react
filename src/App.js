37b1e3a41c59ba721268678cc30240795f4c055d
Wilfried Huss <husswilfried@gmail.com>
2020-05-07 11:02:47 +0200

    move Navigation to its own module

--- src/App.js
+++ src/App.js
@@ -5,63 +5,14 @@ import Toast from "react-bootstrap/Toast";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
 import Table from "react-bootstrap/Table";
-import { LinkContainer } from "react-router-bootstrap";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //import ScriptTag from "react-script-tag";
 
-import "./App.css";
+import Navigation from "./Navigation";
 
-function Navigation() {
-    return (
-        <Navbar className="site-header" bg="dark" expand="lg" variant="dark">
-            <Navbar.Brand><img src="/ReproLight_trans.png" height="50px" alt="REPRO-LIGHT"/></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <LinkContainer to="/">
-                        <Nav.Link>Dashboard</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/devices">
-                        <Nav.Link>Devices</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/about">
-                        <Nav.Link>About</Nav.Link>
-                    </LinkContainer>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <LinkContainer to="/users">
-                            <NavDropdown.Item>Users</NavDropdown.Item>
-                        </LinkContainer>
-                        <NavDropdown.Item href="/plot">Plot</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="/users3">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Statistics" id="basic-nav-dropdown">
-                        <LinkContainer to="/statistics/switch_cycles">
-                            <NavDropdown.Item>On/Off Cycles</NavDropdown.Item>
-                        </LinkContainer>
-                    </NavDropdown>
-                    <NavDropdown title="Database" id="basic-nav-dropdown">
-                        <LinkContainer to="/database_size">
-                            <NavDropdown.Item>Size</NavDropdown.Item>
-                        </LinkContainer>
-                    </NavDropdown>
-                </Nav>
-                {/* <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button>Search</Button>
-                </Form> */}
-            </Navbar.Collapse>
-            <Navbar.Brand><img src="/dot_bartenbach.png" height="50px" alt="Bartenbach"/></Navbar.Brand>
-        </Navbar>
-    );
-}
+import "./App.css";
 
 function AppRouter() {
     return (
@@ -175,44 +126,6 @@ function Plot(props) {
         return <div>Plot is null</div>;
     }
 }
-
-// function PlotDatabaseSize() {
-//     const [error, setError] = useState(null);
-//     const [isLoaded, setIsLoaded] = useState(false);
-//     const [plot_div, setPlotDiv] = useState(null);
-//     const [plot_script, setPlotScript] = useState(null);
-
-//     useEffect(() => {
-//         const script = document.createElement("script");
-
-//         fetch("/backend/plot_database_size")
-//             .then((res) => res.json())
-//             .then(
-//                 (result) => {
-//                     setIsLoaded(true);
-//                     setPlotDiv(result.div);
-//                     setPlotScript(result.script);
-//                 },
-//                 (error) => {
-//                     setIsLoaded(true);
-//                     setError(error);
-//                 }
-//             );
-//     }, []);
-
-//     if (error) {
-//         return <div>Error: {error.message}</div>;
-//     } else if (!isLoaded) {
-//         return <div>Loading ...</div>;
-//     } else {
-//         return (
-//             <>
-//                 <div dangerouslySetInnerHTML={{ __html: plot_div }}></div>
-//                 <ScriptTag type="text/javascript">{plot_script}</ScriptTag>
-//             </>
-//         );
-//     }
-// }
 
 function ExTable() {
     return (
---
+++ src/Navigation.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+//import Form from "react-bootstrap/Form";
+//import FormControl from "react-bootstrap/FormControl";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Navigation() {
+    return (
+        <Navbar className="site-header" bg="dark" expand="lg" variant="dark">
+            <Navbar.Brand>
+                <img src="/ReproLight_trans.png" height="50px" alt="REPRO-LIGHT" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <LinkContainer to="/">
+                        <Nav.Link>Dashboard</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/devices">
+                        <Nav.Link>Devices</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/about">
+                        <Nav.Link>About</Nav.Link>
+                    </LinkContainer>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <LinkContainer to="/users">
+                            <NavDropdown.Item>Users</NavDropdown.Item>
+                        </LinkContainer>
+                        <NavDropdown.Item href="/plot">Plot</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/users3">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Statistics" id="basic-nav-dropdown">
+                        <LinkContainer to="/statistics/switch_cycles">
+                            <NavDropdown.Item>On/Off Cycles</NavDropdown.Item>
+                        </LinkContainer>
+                    </NavDropdown>
+                    <NavDropdown title="Database" id="basic-nav-dropdown">
+                        <LinkContainer to="/database_size">
+                            <NavDropdown.Item>Size</NavDropdown.Item>
+                        </LinkContainer>
+                    </NavDropdown>
+                </Nav>
+                {/* <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button>Search</Button>
+                </Form> */}
+            </Navbar.Collapse>
+            <Navbar.Brand>
+                <img src="/dot_bartenbach.png" height="50px" alt="Bartenbach" />
+            </Navbar.Brand>
+        </Navbar>
+    );
+}
+
+export default Navigation;
