99483ba17ac9b112a2146b0af2d70305c8c04fc2
Wilfried Huss <husswilfried@gmail.com>
2020-05-07 11:42:55 +0200

    useReducer

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,37 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+const dataFetchReducer = (state, action) => {
+    switch (action.type) {
+        case "FETCH_INIT":
+            return {
+                ...state,
+                isLoading: true,
+                isError: false,
+            };
+        case "FETCH_SUCCESS":
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data: action.payload,
+             };
+        case "FETCH_FAILURE":
+            return {
+                ...state,
+                isLoading: false,
+                isError: true,
+            };
+        default:
+            throw new Error();
+    }
+};
+
 const useDataApi = (initialUrl, initialData) => {
-    const [data, setData] = useState(initialData);
     const [url, setUrl] = useState(initialUrl);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-   
+    const [state, dispatch] = useReducer(dataFetchReducer, {
+        isLoading: false,
+        isError: false,
+        data: initialData,
+    });
+
     useEffect(() => {
-      const fetchData = async () => {
-        setIsError(false);
-        setIsLoading(true);
-   
-        try {
-          const result = await axios(url);
-   
-          setData(result.data);
-        } catch (error) {
-          setIsError(true);
-        }
-   
-        setIsLoading(false);
-      };
-   
-      fetchData();
+        const fetchData = async () => {
+            dispatch({ type: "FETCH_INIT" });
+
+            try {
+                const result = await axios(url);
+
+                dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+            } catch (error) {
+                dispatch({ type: "FETCH_FAILURE" });
+            }
+        };
+
+        fetchData();
     }, [url]);
-   
-    return [{ data, isLoading, isError }, setUrl];
-  }
+
+    return [state, setUrl];
+};
 
 function Fetch() {
-    const [{data, isLoading, isError}, doFetch] = useDataApi('https://hn.algolia.com/api/v1/search?query=redux', { hits: []});
-    
+    const [{ data, isLoading, isError }, doFetch] = useDataApi("https://hn.algolia.com/api/v1/search?query=redux", {
+        hits: [],
+    });
+
     return (
         <>
             <span>Fetch</span>
@@ -41,13 +68,13 @@ function Fetch() {
             {isLoading ? (
                 <div>Loading ...</div>
             ) : (
-            <ul>
-                {data.hits.map((item) => (
-                    <li key={item.objectID}>
-                        <a href={item.url}>{item.title}</a>
-                    </li>
-                ))}
-            </ul>
+                <ul>
+                    {data.hits.map((item) => (
+                        <li key={item.objectID}>
+                            <a href={item.url}>{item.title}</a>
+                        </li>
+                    ))}
+                </ul>
             )}
         </>
     );
