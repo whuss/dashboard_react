231b2d3babf794f45629ccf4b524390dbfa53edb
Wilfried Huss <husswilfried@gmail.com>
2020-05-08 14:53:57 +0200

    make input changeable

--- src/SystemLogs.js
+++ src/SystemLogs.js
@@ -43,32 +43,8 @@ function logUrl(device, duration, log_level, timestamp) {
     return baseUrl;
 }
 
-function DevicePicker(props) {
-    const [device, setDevice] = useState(props.device);
-    const [{ data, isLoading, isError }, doFetch] = useDataApi("/backend/devices", []);
 
-    const deviceDropdown = (data) => (
-        <>
-            {data.map((_device) => (
-                <Dropdown.Item key={_device} href={`#${_device}`} onSelect={() => setDevice(_device)}>
-                    {_device}
-                </Dropdown.Item>
-            ))}
-        </>
-    );
-
-    return (
-        <ButtonGroup>
-            <span className="label">Device:</span>
-            <DropdownButton id="dropdown-basic-button" variant="light" title={device}>
-                {isError && <Dropdown.Item>Something went wrong ...</Dropdown.Item>}
-                {isLoading ? <Dropdown.Item>Loading...</Dropdown.Item> : deviceDropdown(data)}
-            </DropdownButton>
-        </ButtonGroup>
-    );
-}
-
-function useDevicePicker(device) {
+function useDevice(device) {
     const [_device, setDevice] = useState(device);
     const [{ data, isLoading, isError }, doFetch] = useDataApi("/backend/devices", []);
 
@@ -117,12 +93,13 @@ function useLogLevel(level) {
 }
 
 function DateTimeInput(props) {
+    const [timestamp, setTimestamp] = useState(props.timestamp);
     return (
         <ButtonGroup>
             <ButtonGroup>
                 <span className="label">At&nbsp;time:</span>
                 <InputGroup className="mb-2">
-                    <FormControl value={props.timestamp} />
+                    <FormControl defaultValue={timestamp} onChange={(e) => setTimestamp(e.target.value)} />
                     <InputGroup.Append>
                         <InputGroup.Text id="basic-addon2">
                             <i className="fa fa-calendar" aria-hidden="true"></i>
@@ -134,20 +111,7 @@ function DateTimeInput(props) {
     );
 }
 
-function DurationPicker(props) {
-    return (
-        <ButtonGroup>
-            <span className="label">Duration:</span>
-            <DropdownButton id="dropdown-basic-button" variant="light" title={props.duration}>
-                <Dropdown.Item href="#/action-1">1 Minute</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">5 Minutes</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">10 Minutes</Dropdown.Item>
-            </DropdownButton>
-        </ButtonGroup>
-    );
-}
-
-function useDurationPicker(_duration) {
+function useDuration(_duration) {
     const [duration, setDuration] = useState(_duration);
     const durations = [1, 5, 10, 30];
 
@@ -172,9 +136,6 @@ function LogToolbar(props) {
     return (
         <Container id="toolbar">
             <ButtonToolbar>
-                <DevicePicker device={props.device} />
-                <DateTimeInput timestamp={props.timestamp} />
-                <DurationPicker duration={props.duration} />
             </ButtonToolbar>
         </Container>
     );
@@ -191,11 +152,11 @@ function SystemLogs() {
     const [_timestamp, setTimestamp] = useState(timestamp);
 
     const [_log_level, setLogLevel] = useLogLevel(log_level);
-    const [_device, setDevice] = useDevicePicker(device);
-    const [_duration, setDuration] = useDurationPicker(duration);
+    const [_device, setDevice] = useDevice(device);
+    const [_duration, setDuration] = useDuration(duration);
 
     const url = logUrl(_device, _duration, _log_level, _timestamp);
-    
+
     return (
         <>
             <Toolbar>
