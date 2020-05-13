f46f3a8c9707a9e523b641cf9e3d04610f22f94b
Wilfried Huss <husswilfried@gmail.com>
2020-05-13 11:01:31 +0200

    load all distribuation plots separately

--- src/ClusteringInputDistributions.js
+++ src/ClusteringInputDistributions.js
@@ -3,6 +3,9 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 import DeviceTable from "./DeviceTable";
 
@@ -14,11 +17,28 @@ import { downloadFile } from "./Fetch";
 
 import { useDropdown } from "./Toolbar";
 
-const columns = ['key_press_count', 'delete_press_count', 'enter_press_count', 'shift_press_count',
-                 'space_press_count', 'press_pause_count', 'pause_length', 'keystroke_time', 
-                 'press_to_press_time', 'click_count', 'double_click_count', 'rotation_distance',
-                 'rotation_speed', 'event_count', 'gesture_distance', 'gesture_speed',
-                 'gesture_deviation', 'gesture_duration_seconds'];
+const columns = [
+    "key_press_count",
+    "delete_press_count",
+    "enter_press_count",
+    "shift_press_count",
+    "space_press_count",
+    "press_pause_count",
+    "pause_length",
+    "keystroke_time",
+    "press_to_press_time",
+    "click_count",
+    "double_click_count",
+    "rotation_distance",
+    "rotation_speed",
+    "event_count",
+    "gesture_distance",
+    "gesture_speed",
+    "gesture_deviation",
+    "gesture_duration_seconds",
+];
+
+//const columns = ['key_press_count', 'delete_press_count'];
 
 function clusteringUrl(device, transformation) {
     const baseUrl = "/backend/plot_cluster_input";
@@ -29,7 +49,7 @@ function useClusteringToolbar(_transformation) {
     if (!_transformation) {
         _transformation = "none";
     }
-    
+
     const [transformation, setTransformation] = useDropdown(_transformation, {
         values: ["none", "power transform"],
         label: "Transformation",
@@ -37,11 +57,7 @@ function useClusteringToolbar(_transformation) {
 
     //const params = `/clustring/input_distributation/${transformation}`;
 
-    const sensorToolbar = (
-        <>
-            {setTransformation}
-        </>
-    );
+    const sensorToolbar = <>{setTransformation}</>;
 
     const plotUrl = (device) => clusteringUrl(device, transformation);
 
@@ -55,30 +71,48 @@ function useClusteringToolbar(_transformation) {
     return [sensorToolbar, plotUrl, plot_parameters];
 }
 
+const DistributionPlot = (props) => {
+    const plot_name = "PlotClusteringInputDistribution";
+    var plot_parameters = props.plot_parameters;
+    plot_parameters.column = props.column;
+
+    const [{ fields, isLoading, isError, errorMsg }, plot] = usePlot(plot_name, plot_parameters);
+
+    return (
+        <>
+            <Col>
+            <LoadingAnimation isLoading={isLoading} isError={isError} errorMsg={errorMsg}>
+                {plot}
+            </LoadingAnimation>
+            </Col>
+        </>
+    );
+};
+
 function rowFactory(plotUrl, plot_parameters) {
     const TableRow = (props) => {
-        const device = props.device_id
+        const device = props.device_id;
         const plot_name = "PlotClusteringInputDistribution";
         const file_name = `clustering_input_distribution_${props.device_id}.xlsx`;
-        const [{ fields, isLoading, isError, errorMsg }, plot] = usePlot(plot_name, plot_parameters(device));
 
         return (
-        <>
-            <td>
-                <LoadingAnimation isLoading={isLoading} isError={isError} errorMsg={errorMsg}>
-                    {plot}
-                </LoadingAnimation>
-            </td>
-            <td>
-                <Button
-                    onClick={() =>
-                        downloadFile(plot_name, plot_parameters(device), file_name)
-                    }
-                >
-                    Download
-                </Button>
-            </td>
-        </>);
+            <>
+                <td>
+                    <Container>
+                        <Row>
+                    {columns.map((column) => (
+                        <DistributionPlot key={column} plot_parameters={plot_parameters(device)} column={column} />
+                    ))}
+                    </Row>
+                    </Container>
+                </td>
+                <td>
+                    <Button onClick={() => downloadFile(plot_name, plot_parameters(device), file_name)}>
+                        Download
+                    </Button>
+                </td>
+            </>
+        );
     };
 
     return TableRow;
@@ -97,14 +131,13 @@ const ClusteringInputDistribution = (props) => {
     const TableRow = rowFactory(plotUrl, plot_parameters);
 
     return (
-    <>
-        <p>
-            <b>Warning:</b> Plotting of the distributions can take a very long time.
-        </p>
-        <DeviceTable format_header={TableHeader} format_row={TableRow} toolbar={tools} devices={props.devices}/>;
-    </>
-);
-}
+        <>
+            <p>
+                <b>Warning:</b> Plotting of the distributions can take a very long time.
+            </p>
+            <DeviceTable format_header={TableHeader} format_row={TableRow} toolbar={tools} devices={props.devices} />;
+        </>
+    );
+};
 
 export default ClusteringInputDistribution;
-
