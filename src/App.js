d29b358c20abdc4a250323c70793ee22090ff678
Wilfried Huss <husswilfried@gmail.com>
2020-05-19 08:07:35 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
 
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 import Jumbotron from "react-bootstrap/Jumbotron";
-import Toast from "react-bootstrap/Toast";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import Alert from "react-bootstrap/Alert";
-
-import useDateRange, { formatDate } from "./DatePicker";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -69,25 +64,25 @@ const MainView = (props) => {
                     path={["/analytics/sensor/:device/:sensor/:sample_rate/:start_date/:end_date", "/analytics/sensor"]}
                     title="Analytics Sensor"
                 >
-                    <AnalyticsSensor devices={devices}/>
+                    <AnalyticsSensor devices={devices} />
                 </NavRoute>
                 <NavRoute path="/analytics/scenes" title="Analytics Scenes">
-                    <AnalyticsScenes devices={devices}/>
+                    <AnalyticsScenes devices={devices} />
                 </NavRoute>
                 <NavRoute path="/analytics/power" title="Analytics Power timeline">
-                    <AnalyticsPowerTimeline devices={devices}/>
+                    <AnalyticsPowerTimeline devices={devices} />
                 </NavRoute>
                 <NavRoute path="/analytics/connection" title="Analytics Connection">
-                    <AnalyticsConnection devices={devices}/>
+                    <AnalyticsConnection devices={devices} />
                 </NavRoute>
                 <NavRoute path="/analytics/keyboard" title="Analytics Keyboard">
-                    <AnalyticsKeyboard devices={devices}/>
+                    <AnalyticsKeyboard devices={devices} />
                 </NavRoute>
                 <NavRoute path="/analytics/keypress" title="Analytics Keypress">
-                    <AnalyticsKeypress devices={devices}/>
+                    <AnalyticsKeypress devices={devices} />
                 </NavRoute>
                 <NavRoute path="/analytics/mouse" title="Analytics Mouse">
-                    <AnalyticsMouse devices={devices}/>
+                    <AnalyticsMouse devices={devices} />
                 </NavRoute>
                 <NavRoute path="/clustering/input_distributions" title="Clustering Input Distributions">
                     <ClusteringInputDistributions devices={devices} />
@@ -102,16 +97,16 @@ const MainView = (props) => {
                     <ClusteringTimeline devices={devices} />
                 </NavRoute>
                 <NavRoute path="/statistics/switch_cycles" title="Statistics On/Off Cycles">
-                    <SwitchCycles devices={devices}/>
+                    <SwitchCycles devices={devices} />
                 </NavRoute>
                 <NavRoute path="/system/stability" title="System Stability">
-                    <SystemStability devices={devices}/>
+                    <SystemStability devices={devices} />
                 </NavRoute>
                 <NavRoute path="/system/restarts" title="System Restarts">
-                    <SystemRestarts devices={devices}/>
+                    <SystemRestarts devices={devices} />
                 </NavRoute>
                 <NavRoute path="/system/errors" title="System Errors">
-                    <SystemErrors devices={devices}/>
+                    <SystemErrors devices={devices} />
                 </NavRoute>
                 <NavRoute path="/database_size" title="Database Size">
                     <Plot src="/backend/plot_database_size" />
@@ -132,63 +127,30 @@ const MainView = (props) => {
                     <DeviceDetails devices={devices} />
                 </NavRoute>
                 <NavRoute path="/" title="Dashboard">
-                    <Dashboard devices={devices}/>
+                    <Dashboard devices={devices} />
                 </NavRoute>
             </Switch>
         </Container>
     );
-}
+};
 
 function AppRouter() {
     const [devices, isLoading, isError] = useDevice();
     return (
         <Router>
             <NavPortal />
-            <LoadingAnimation isLoading={isLoading} isError={isError}><MainView devices={devices}/></LoadingAnimation>
+            <LoadingAnimation isLoading={isLoading} isError={isError}>
+                <MainView devices={devices} />
+            </LoadingAnimation>
         </Router>
     );
 }
 
-function Example() {
-    return (
-        <div>Altert</div>
-        // s
-    );
-}
-
-const ExampleToast = ({ children }) => {
-    const [show, toggleShow] = useState(true);
-
-    return (
-        <>
-            {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-            <Toast show={show} onClose={() => toggleShow(false)}>
-                <Toast.Header>
-                    <strong className="mr-auto">React-Bootstrap</strong>
-                </Toast.Header>
-                <Toast.Body>{children}</Toast.Body>
-            </Toast>
-            <Example />
-        </>
-    );
-};
-
 function About() {
-    const [from, to, dateRange] = useDateRange(new Date("2020-04-05T00:00:00"), new Date("2020-05-04T00:00:00"));
-
     return (
-        <div>
-            {dateRange}
-            <Jumbotron>
-                <h1 className="header">Welcome To React-Bootstrap</h1>
-                <ExampleToast>
-                    We now have Bootstrap
-                    <span role="img" aria-label="tada">
-                        🎉
-                    </span>
-                </ExampleToast>
-            </Jumbotron>
-        </div>
+        <Jumbotron>
+            <h1 className="header">Todo ...</h1>
+        </Jumbotron>
     );
 }
 
