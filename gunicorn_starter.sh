11f37b15049b2d759b0c5c50833c17db4039d56b
Wilfried Huss <wilfried.huss@bartenbach.com>
2020-05-26 12:38:40 +0200

    increase worker timeout to 3 minutes

--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn  --error-logfile err.log --access-logfile access.log --log-level debug app:app --worker-class=gevent --workers=9 --worker-connections=1000 -b 0.0.0.0:8003
+gunicorn  --error-logfile err.log --access-logfile access.log --log-level debug app:app --worker-class=gevent --workers=9 --worker-connections=1000 --timeout 180 --graceful-timeout 180 -b 0.0.0.0:5000
