19c7104d4ec8e7483ff0c86c46d8d0348f69933d
Wilfried Huss <husswilfried@gmail.com>
2020-05-27 18:51:58 +0200

    support environment variable to set the host of the cache database

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,5 @@
+import os
+
 # ----------------------------------------------------------------------------------------------------------------------
 # Configuration
 # ----------------------------------------------------------------------------------------------------------------------
@@ -8,13 +10,14 @@ _host: str = "83.175.125.85"
 _user: str = "infinity"
 _password: str = "iGe9kH9j"
 _dbname: str = "bbf_inf_rep"
+_cache_host: str = os.getenv('CACHE_DB', "127.0.0.1")
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 
 class Config:
     db_url: str = f'mysql://{_user}:{_password}@{_host}/{_dbname}'
-    db_cache_url: str = f'mysql://{_user}:{_password}@127.0.0.1/bbf_inf_cache'
+    db_cache_url: str = f'mysql://{_user}:{_password}@{_cache_host}/bbf_inf_cache'
     debug: bool = True
     update_cache: str = "expired"  # always, never, expired
     secret: bytes = b'mdmekrifglvkcmalqwoeircbnfkhgpolc'
