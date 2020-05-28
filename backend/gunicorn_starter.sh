6db08cc899586063666ac1a46b70cc5634afc973
Wilfried Huss <wilfried.huss@bartenbach.com>
2020-05-28 12:20:13 +0200

    format script

--- backend/gunicorn_starter.sh
+++ backend/gunicorn_starter.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-gunicorn  --error-logfile err.log --access-logfile access.log --log-level debug app:app --worker-class=gevent --workers=9 --worker-connections=1000 --timeout 180 --graceful-timeout 180 -b 0.0.0.0:5000
+gunicorn  --error-logfile err.log \
+          --access-logfile access.log \
+          --log-level debug app:app \
+          --worker-class=gevent \
+          --workers=9 \
+          --worker-connections=1000 \
+          --timeout 180 \
+          --graceful-timeout 180 \
+          -b 0.0.0.0:5000
