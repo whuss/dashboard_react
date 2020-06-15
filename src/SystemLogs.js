92e49e7e011c7fca720aa11efb3a6cb9f7e74dbe
Wilfried Huss <husswilfried@gmail.com>
2020-06-15 09:44:07 +0200

    refactor

--- src/SystemLogs.js
+++ src/SystemLogs.js
@@ -10,65 +10,8 @@ import useDataApi from "./Fetch";
 import Toolbar, { ToolbarBottom, useDropdown, useDeviceDropdown, useTimestamp, LoadingAnimation } from "./Toolbar";
 
 import Container from "react-bootstrap/Container";
-import Pagination from "react-bootstrap/Pagination";
-import PageItem from "react-bootstrap/PageItem";
 
-function PaginationBar(props) {
-    const pagination = props.pagination;
-    const setPage = props.setPage;
-
-    if (!pagination) {
-        return <></>;
-    }
-
-    const { current_page, num_pages, has_next, has_prev, next_num, prev_num, pages } = pagination;
-
-    if (num_pages <= 1) {
-        return <></>;
-    }
-
-    function clickPage(page) {
-        console.log("click page: ", page);
-        setPage(page);
-    }
-
-    function prevPage() {
-        console.log("prev page");
-        if (has_prev) {
-            setPage(current_page - 1);
-        }
-    }
-
-    function nextPage() {
-        console.log("next page");
-        if (has_next) {
-            setPage(current_page + 1);
-        }
-    }
-
-    function item(page) {
-        if (page) {
-            return (
-                <Pagination.Item onClick={() => clickPage(page)} key={page} active={page === current_page}>
-                    {page}
-                </Pagination.Item>
-            );
-        }
-        return (
-            <Pagination.Item key="ellipsis" disabled={true} active={false}>
-                &hellip;
-            </Pagination.Item>
-        );
-    }
-
-    return (
-        <Pagination size="sm">
-            <Pagination.Prev  onClick={() => prevPage()} disabled={!has_prev} />
-            {pages.map((page) => item(page))}
-            <Pagination.Next onClick={() => nextPage()} disabled={!has_next} />
-        </Pagination>
-    );
-}
+import PaginationBar from "./PaginationBar";
 
 function useLog(url) {
     const [{ data, isLoading, isError }, doFetch] = useDataApi(url, []);
--- src/SystemRestarts.js
+++ src/SystemRestarts.js
@@ -9,65 +9,7 @@ import DeviceTable from "./DeviceTable";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeartbeat } from "@fortawesome/free-solid-svg-icons";
 
-import Pagination from "react-bootstrap/Pagination";
-import PageItem from "react-bootstrap/PageItem";
-
-function PaginationBar(props) {
-    const pagination = props.pagination;
-    const setPage = props.setPage;
-
-    if (!pagination) {
-        return <></>;
-    }
-
-    const { current_page, num_pages, has_next, has_prev, next_num, prev_num, pages } = pagination;
-
-    if (num_pages <= 1) {
-        return <></>;
-    }
-
-    function clickPage(page) {
-        console.log("click page: ", page);
-        setPage(page);
-    }
-
-    function prevPage() {
-        console.log("prev page");
-        if (has_prev) {
-            setPage(current_page - 1);
-        }
-    }
-
-    function nextPage() {
-        console.log("next page");
-        if (has_next) {
-            setPage(current_page + 1);
-        }
-    }
-
-    function item(page) {
-        if (page) {
-            return (
-                <Pagination.Item onClick={() => clickPage(page)} key={page} active={page === current_page}>
-                    {page}
-                </Pagination.Item>
-            );
-        }
-        return (
-            <Pagination.Item key="ellipsis" disabled={true} active={false}>
-                &hellip;
-            </Pagination.Item>
-        );
-    }
-
-    return (
-        <Pagination size="sm">
-            <Pagination.Prev  onClick={() => prevPage()} disabled={!has_prev} />
-            {pages.map((page) => item(page))}
-            <Pagination.Next onClick={() => nextPage()} disabled={!has_next} />
-        </Pagination>
-    );
-}
+import PaginationBar from "./PaginationBar";
 
 const RestartIcon = (props) => {
     const status = props.crash ? "SICK" : "HEALTHY";
