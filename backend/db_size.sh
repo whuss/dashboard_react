ce90ac1dba86adb003be98e712f7458cf776b6b7
Wilfried Huss <wilfried.huss@bartenbach.com>
2020-07-06 11:26:32 +0200

    cleanup

--- backend/db_size.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-ROOT_DIR=$(dirname "$0")
-cd $ROOT_DIR
-
-source ~/.virtualenvs/infinity/bin/activate
-
-./db_size.py
--- backend/jobber_starter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p /jobber_logs/errors && \
-mkdir -p /jobber_logs/success && \
-jobberrunner /var/lib/jobber/.jobber
